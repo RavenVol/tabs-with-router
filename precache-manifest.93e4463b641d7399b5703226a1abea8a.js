522175dd980c53b6fec70ab1611dbea36dcc239e
RavenVol <r.volianiuk@gmail.com>
2019-07-31 17:17:35 +0300

    Updates

---
+++ precache-manifest.93e4463b641d7399b5703226a1abea8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c5107e8d1eb2ae8917547a7defd508",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "f4d442e1941b78253b47",
+    "url": "/react_tabs-with-router/static/css/main.e2a2c9e0.chunk.css"
+  },
+  {
+    "revision": "d76516c64d368ddb7a64",
+    "url": "/react_tabs-with-router/static/js/2.8957dc25.chunk.js"
+  },
+  {
+    "revision": "f4d442e1941b78253b47",
+    "url": "/react_tabs-with-router/static/js/main.6299212e.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
