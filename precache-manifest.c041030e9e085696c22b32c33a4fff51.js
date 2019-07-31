dcddd46d754a456cbd962ce159e940661e4b5f14
RavenVol <r.volianiuk@gmail.com>
2019-07-31 17:36:18 +0300

    Updates

--- precache-manifest.c041030e9e085696c22b32c33a4fff51.js
+++ precache-manifest.c041030e9e085696c22b32c33a4fff51.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "768181939d35654972255740f7be976a",
+    "revision": "8ab9d978975c10e88f76a3722ffd03ba",
     "url": "/react_tabs-with-router/index.html"
   },
   {
-    "revision": "415e7dbab1a440dd96ff",
+    "revision": "08c07c3e43b6a3f0333f",
     "url": "/react_tabs-with-router/static/css/main.e2a2c9e0.chunk.css"
   },
   {
-    "revision": "d76516c64d368ddb7a64",
-    "url": "/react_tabs-with-router/static/js/2.8957dc25.chunk.js"
+    "revision": "a42343673fe86aae9ed0",
+    "url": "/react_tabs-with-router/static/js/2.6a962d7b.chunk.js"
   },
   {
-    "revision": "415e7dbab1a440dd96ff",
-    "url": "/react_tabs-with-router/static/js/main.73b0cee6.chunk.js"
+    "revision": "08c07c3e43b6a3f0333f",
+    "url": "/react_tabs-with-router/static/js/main.eb5216e5.chunk.js"
   },
   {
     "revision": "da3ca6fceb301548d22d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_tabs-with-router/precache-manifest.d099bec55cc4d3ffc1e739f7489cf911.js"
+  "/react_tabs-with-router/precache-manifest.c041030e9e085696c22b32c33a4fff51.js"
 );
 
 self.addEventListener('message', (event) => {
